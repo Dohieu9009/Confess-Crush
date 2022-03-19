9edb5c6765c09caa1c01d81f8619504197d1cb01
Dohieu9009 <101917890+Dohieu9009@users.noreply.github.com>
2022-03-19 13:00:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi người yêu anh :333333",
+    introTitle: 'Nguyễn Cẩm Tiên',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
