823c9b5bcd135b2284d8f379cc7c27e9910ff044
Dohieu9009 <101917890+Dohieu9009@users.noreply.github.com>
2022-03-19 13:14:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '         ><HeHe><           ',
+    btnIntro: '><HeHe><',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
